beca74d4911e1e102100d507cc10ca72820e1ae2
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-07-13 13:37:23 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,6 +9,6 @@ const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/").post(registerUser).get(protect, allUsers);
-router.put("/login", authUser);
+router.get("/login", authUser);
 
 module.exports = router;
