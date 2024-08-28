8d16c2a015611259c1b8dc79fccef11ff3d99fd7
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-08-28 21:53:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://localhost:3000",
+    origin: "http://localhost:3000",
   },
 });
 
