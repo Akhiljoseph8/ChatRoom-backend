8978a0bcba1b7a85b028b04eefb0c805fb067b03
Akhil Joseph <120910561+Akhiljoseph8@users.noreply.github.com>
2024-08-28 21:34:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "chat-room-calv.vercel.app",
+    origin: "https://chat-room-calv.vercel.app",
   },
 });
 
